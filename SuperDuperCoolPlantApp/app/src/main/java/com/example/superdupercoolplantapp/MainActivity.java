d81a37be737209200762793643263f48a5c0e537
elYonno <78961541+elYonno@users.noreply.github.com>
2022-02-28 20:35:33 +0100

    created working bottom navigation menu

--- SuperDuperCoolPlantApp/app/src/main/java/com/example/superdupercoolplantapp/MainActivity.java
+++ SuperDuperCoolPlantApp/app/src/main/java/com/example/superdupercoolplantapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.superdupercoolplantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set up bottom navigation menu
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
